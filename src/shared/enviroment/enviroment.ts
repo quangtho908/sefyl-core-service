4749cf03d3a003090df48c83c919ab1aa6926eac
quangtho908 <quangtho23062002@gmail.com>
2021-09-01 09:44:51 +0700

    update enviroment variables

--- src/shared/enviroment/enviroment.ts
+++ src/shared/enviroment/enviroment.ts
@@ -1,30 +1,32 @@
+import { config } from "dotenv";
+config();
 export const env = {
     app: {
-        host: "localhost",
-        port: 3000,
-        domain: "http://localhost:3000"
+        host: process.env.HOST || "localhost",
+        port: process.env.PORT || 3000 ,
+        domain: process.env.DOMAIN || "http://localhost:3000"
     },
     collections: {
-        users: "users",
-        masterData: "masterData"
+        users: process.env.COLLECTION_USERS,
+        masterData: process.env.COLLECTION_MASTERDATA
     },
     mongo: {
-        uri: "mongodb+srv://sefyl:23062002@cluster0.c0pmn.mongodb.net/sefyl?retryWrites=true&w=majority"
+        uri: process.env.DB_URI
     },
     auth: {
-        secret: "8303becf997551ee56ee93b4ba76f19c8960c3da51fef4bc05d76b50f40faebe",
+        secret: process.env.SECRET,
         expired: 24,
         salt: 10,
-        secretRefresh: "6d1bff57df6a938a16d644091c0acc4adb10108c79a225fa83af2d79c86be174",
+        secretRefresh: process.env.SECRET_REFRESH,
         expiredRefresh: 720,
     },
     uploads: {
         dirname: __dirname.split("\\").slice(0, -3).join("/"),
-        avatars: "/uploads/avatars" 
+        avatars: process.env.AVATARS
     },
     redis: {
-        host: "redis-19989.c252.ap-southeast-1-1.ec2.cloud.redislabs.com",
-        port: 19989,
-        password: "K8UfPtYsxLi9MYWW0vZcH5QzdWTx3IPJ"
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT),
+        password: process.env.REDIS_PASS
     }
 }
