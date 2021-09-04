d12b75e3a4f3e6fd373c9d3a60dd969ccc5945c8
quangtho908 <quangtho23062002@gmail.com>
2021-09-04 15:16:17 +0700

    update

--- src/shared/decorators/user.ts
+++ src/shared/decorators/user.ts
@@ -3,8 +3,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const User = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    request.user = { id: "6124e31d80801213e06d45e8" , role: "User",
-        refreshToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMjRlMzFkODA4MDEyMTNlMDZkNDVlOCIsInJvbGUiOiJVc2VyIiwiaWF0IjoxNjMwMjA0MTE4LCJleHAiOjE2MzAyMDQ4Mzh9.1QU29JZ8frnxN-Q-BA6ULKa6m6bKnnVbJV0P3ohuFLs"}
     return request.user;
   },
 );
